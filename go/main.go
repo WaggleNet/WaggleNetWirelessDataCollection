0b3f0febb80361bce8bc0d6367d79826227ecad3
Theodore Josephson <tdj4@illinois.edu>
2024-04-04 20:04:06 -0500

    added accumulator string

--- go/main.go
+++ go/main.go
@@ -2,48 +2,31 @@ package main
 
 import (
     "fmt"
-    "io"
-	"os"
     "net/http"
+    "io/ioutil"
 )
 
+var s = ""
+
 func uploadFile(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("File Upload Endpoint Hit")
-
-    // Parse our multipart form, 10 << 20 specifies a maximum
-    // upload of 10 MB files.
-    r.ParseMultipartForm(10 << 20)
-    // FormFile returns the first file for the given key `myFile`
-    // it also returns the FileHeader so we can get the Filename,
-    // the Header and the size of the file
-    file, handler, err := r.FormFile("myFile")
-    if err != nil {
-        fmt.Println("Error Retrieving the File")
-        fmt.Println(err)
-        return
-    }
-    defer file.Close()
-    fmt.Printf("Uploaded File: %+v\n", handler.Filename)
-    fmt.Printf("File Size: %+v\n", handler.Size)
-    fmt.Printf("MIME Header: %+v\n", handler.Header)
-
-	// Open the destination file in append mode
-    destFile, err := os.OpenFile("temp/upload.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-    if err != nil {
-        fmt.Println("Error opening destination file")
-		fmt.Println(err)
-        return
-    }
-    defer destFile.Close()
-
-    // Copy the uploaded file's content directly to upload.txt
-    if _, err := io.Copy(destFile, file); err != nil {
-        fmt.Println("Error saving file")
-		fmt.Println(err)
-        return
-    }
-
-    fmt.Fprintf(w, "Successfully Uploaded File\n")
+        // Read the body of the request
+        body, err := ioutil.ReadAll(r.Body)
+        if err != nil {
+            http.Error(w, "Error reading request body", http.StatusBadRequest)
+            return
+        }
+    
+        // Convert the body to a string
+        bodyString := string(body)
+    
+        // Print the body string to the console
+        //fmt.Println("Request Body:", bodyString)
+        s += bodyString
+        fmt.Println(s)
+    
+        // Send a response
+        fmt.Fprintf(w, "Received request body:\n%s", bodyString)
+    
 }
 
 func setupRoutes() {
