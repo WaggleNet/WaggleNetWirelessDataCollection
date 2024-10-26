39ba02ac912c918b73b35fcdd6b8b5e8a9f556b8
xu.z.brandon@gmail.com <68095440+bxrr@users.noreply.github.com>
2024-10-26 11:45:14 -0500

    additional mqtt client testing

--- mqtt-test/mqtt-test.py
+++ mqtt-test/mqtt-test.py
@@ -3,10 +3,15 @@ import time
 
 m = MQTT('localhost', 1883)
 
-m.subscribe("/test-sensor")
+def mqtt_callback(client, userdata, msg):
+    print(msg.payload)
+    print(userdata)
+    print(client)
+
+m.subscribe("/test-sensor", mqtt_callback)
 time.sleep(1)
 
-for i in range(100):
+for i in range(3):
     m.publish("/test-sensor", "testmessage")
 
 time.sleep(1)
