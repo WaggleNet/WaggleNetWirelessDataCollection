ccf41c5577b2f7223dcf7372d6d66d57894004d5
ted <tdj4@illinois.edu>
2024-10-31 20:12:23 -0500

    removed garbage code and added comments to say what the garbge code's replacement should do

--- network-client/main.py
+++ network-client/main.py
@@ -22,46 +22,17 @@ rfm9x.enable_crc = False
 rfm9x.node = 201
 rfm9x.destination = 200
 
-msg = ""
-msg_list = ""
-HTML_start = "<!DOCTYPE HTML>\n<html><body>"
-HTML_end = "</body></html>"
-file = open("received.html", "w")
-keyfile = open('key.txt')
-
-access_key = keyfile.readline()
-secret_key = keyfile.readline()
-
-bucket_name = 'beehive-data-1.0'
-
-last_upload = time.time()
-
-def upload(filepath, stamp):
-    s3 = boto3.client('s3', aws_access_key_id = access_key, aws_secret_access_key=secret_key)
-    s3.upload_file(filepath, bucket_name, f'{datetime.now()}-{stamp}')
-
-num_upload = 0
-
 while True:
     msg = rfm9x.receive(keep_listening = True, with_header=True, with_ack=True)
+    # pull data off radio
+
     if(msg is not None):
-        print(msg, time.time())
-        file.truncate(0)
+        # deserialize
 
-        now = datetime.now()
+        # publish to mqtt
+        pass
+    
+    print(f"Rssi: {rfm9x.last_rssi}")
 
-        newline = str(now) + ": " + msg.decode(errors='ignore') + "<br>"
-        msg_list = newline + msg_list
-        file.write(HTML_start + msg_list[0:1000] + HTML_end)
-        file.flush()
-    else:
-        print(f"nothing yet... Rssi: {rfm9x.last_rssi}")
-        
-    print(f"Rssi: {rfm9x.last_rssi}")	
 
-    if time.time() - last_upload > 20:
-        upload("received.html", num_upload)
-
-        num_upload += 1
-        last_upload = time.time()
     
