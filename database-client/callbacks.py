e19407eff08b3eb5d8789de7c2d84838654df92d
xu.z.brandon@gmail.com <68095440+bxrr@users.noreply.github.com>
2025-01-23 20:01:30 -0600

    basic debugging prints for db client

--- database-client/callbacks.py
+++ database-client/callbacks.py
@@ -4,3 +4,4 @@ import database
 def sensor_data_callback(client, userdata, msg) -> None:
     data = json.loads(msg.payload)
     database.upload_sensor_data(data['hive_id'], data['sensor_id'], data['timestamp'], data['value'])
+    print(client, userdata, data)
--- database-client/client.py
+++ database-client/client.py
@@ -1,25 +1,18 @@
 from mqtt import MQTT
 import callbacks
-import time, json
+import time
 
 HOST = "localhost"
 PORT = 1883
 
 client = MQTT(HOST, PORT)
 
-data = {
-        'hive_id': "12345test", # identifying string for which hive
-        'sensor_id': "s1", # identifying string for which sensor within a hive
-        'timestamp': 149129421, # unix timestamp in ms of when the data was retrieved
-        'value': 12543.0, # sensor value
-    }
-
-data_json = json.dumps(data)
-
-def start_client():
+def start_client() -> None:
     client.subscribe("/test-sensor", callbacks.sensor_data_callback)
-    client.publish("/test-sensor", data_json)
    
     print("Listening to MQTT broker for new data, CTRL-C to exit")
     while True:
         time.sleep(1)
+
+if __name__ == "__main__":
+    start_client()
